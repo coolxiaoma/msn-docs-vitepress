8c64b65c3a50fa399b15e1e1193c9d029b85f6e9
马思宁 <598138645@qq.com>
2025-09-20 14:37:51 +0800

    部署

---
+++ docs/assets/README.md.BUyUQ5fR.js
@@ -0,0 +1,18 @@
+import{_ as a,c as n,o as p,ag as e}from"./chunks/framework.B4sg4S7y.js";const _=JSON.parse('{"title":"🚀文件目录结构","description":"","frontmatter":{},"headers":[],"relativePath":"README.md","filePath":"README.md"}'),l={name:"README.md"};function t(i,s,c,o,d,r){return p(),n("div",null,[...s[0]||(s[0]=[e(`<h1 id="🚀文件目录结构" tabindex="-1">🚀文件目录结构 <a class="header-anchor" href="#🚀文件目录结构" aria-label="Permalink to &quot;🚀文件目录结构&quot;">​</a></h1><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>├── docs 打包后的文件</span></span>
+<span class="line"><span>│   ├── index.md</span></span>
+<span class="line"><span>│   ├── markdown-examples.md</span></span>
+<span class="line"><span>│   ├── front-end_react-examples.md</span></span>
+<span class="line"><span>│   ├── front-end_vue-examples.md</span></span>
+<span class="line"><span>│   └── api-examples.md</span></span>
+<span class="line"><span>├── .vitepress 配置文件</span></span>
+<span class="line"><span>│   ├── config.js</span></span>
+<span class="line"><span>│   └── theme</span></span>
+<span class="line"><span>│       └── Layout.vue</span></span>
+<span class="line"><span>├── front-end 前端文件</span></span>
+<span class="line"><span>│   ├── react</span></span>
+<span class="line"><span>│   └── vue</span></span>
+<span class="line"><span>├── back-end 后端文件</span></span>
+<span class="line"><span>├── package.json</span></span>
+<span class="line"><span>├── README.md</span></span>
+<span class="line"><span>├── utils</span></span>
+<span class="line"><span>│   └── auto-gen-sidebar.mjs</span></span></code></pre></div>`,2)])])}const u=a(l,[["render",t]]);export{_ as __pageData,u as default};
